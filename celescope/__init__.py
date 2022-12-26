6daf8478098862bac67c3dd2d5fa5990c02e8175
Chenjunjie1996 <74704889+Chenjunjie1996@users.noreply.github.com>
2022-12-26 09:44:38 +0800

    Atac (#215)
    * atac for sgr pattern
    
    * lint
    
    * docs
    
    * Dev (#3)
    
    * revert discard_UMI
    
    * update change log
    
    Co-authored-by: zhouyiqi <zhouyiqi@singleronbio.com>
    
    Co-authored-by: 陈俊杰 <chenjunjie@SGRNJshiny.sgr.com>
    Co-authored-by: 陈俊杰 <chenjunjie@SGRNJlogin.sgr.com>
    Co-authored-by: zhouyiqi <zhouyiqi@singleronbio.com>

--- celescope/__init__.py
+++ celescope/__init__.py
@@ -1,7 +1,7 @@
 import os
 from collections import OrderedDict
 
-__VERSION__ = "1.14.0b0"
+__VERSION__ = "1.14.0"
 __version__ = __VERSION__
 
 ASSAY_LIST = [
@@ -20,7 +20,8 @@ ASSAY_LIST = [
     'flv_trust4',
     'sweetseq',
     'rna_virus',
-    'utils'
+    'utils',
+    'atac'
 ]
 
 ROOT_PATH = os.path.dirname(__file__)
