2bca05e6b0ea645e98e8c344417e9d7912bbe722
陈俊杰 <chenjunjie@singleronbio.com>
2023-07-26 17:24:36 +0800

    pylint

--- celescope/bulk_vdj/count_vdj.py
+++ celescope/bulk_vdj/count_vdj.py
@@ -234,15 +234,15 @@ class Count_vdj(Step):
         final_df_aa.to_csv(self.clonetypes_aa, sep=',', index=False)
         final_df_nt.to_csv(self.clonetypes_nt, sep=',', index=False)
         
+        df_table = final_df_aa.groupby('Index').head(100)
+        df_table = df_table[["Index", "ClonotypeID", "aaSeqCDR3", "Frequency", "Proportion", "Diversity"]]
+        
         mean_diversity = round(np.mean(df_table.Diversity), 2)
         self.add_metric(
             name="Mean Diversity",
             value=mean_diversity,
             help_info="Mean diversity of all indexes"
         )
-        
-        df_table = final_df_aa.groupby('Index').head(100)
-        df_table = df_table[["Index", "ClonotypeID", "aaSeqCDR3", "Frequency", "Proportion", "Diversity"]]
         
         table_dict = self.get_table_dict(
             title="Clonotypes by Index",
